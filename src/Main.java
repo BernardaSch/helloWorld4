27d1b54a95b9dfa13be8b7b9e367707b7049490e
Bernarda <bernarda.schwalba@gmail.com>
2025-03-21 19:56:45 +0100

    spremanje na grana4

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Dodana treca linija!");
         System.out.println("Sada radim promjene na nova_grana3!");
         System.out.println("Sada želim da promjena bude samo na nova_grana3!");
+        System.out.println("Sada sam na grana4");
     }
 }
