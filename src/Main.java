1384f4782fc5a46b7f244e65c19f940e6bb1f9ba
Bernarda <bernarda.schwalba@gmail.com>
2025-03-21 19:48:38 +0100

    comitanje promjena na nova_grana3

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Dodana nova linija!");
         System.out.println("Dodana treca linija!");
         System.out.println("Sada radim promjene na nova_grana3!");
+        System.out.println("Sada želim da promjena bude samo na nova_grana3!");
     }
 }
