7856eefbec0b74f9c17106cd10619d69b60cdbec
Bernarda <bernarda.schwalba@gmail.com>
2025-03-21 21:42:26 +0100

    Commit na nova_grana3

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Dodana treca linija!");
         System.out.println("Sada radim promjene na nova_grana3!");
         System.out.println("Sada želim da promjena bude samo na nova_grana3!");
+        System.out.println("Želim da ovo bude na masteru.");
     }
 }
