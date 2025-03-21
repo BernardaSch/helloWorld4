afdcb4fd98aa5d67e3137205aba3abd164dab266
Bernarda <bernarda.schwalba@gmail.com>
2025-03-21 19:41:31 +0100

    commit za nova_grana3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Dodana nova linija!");
+        System.out.println("Dodana treca linija!");
     }
 }
